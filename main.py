9696e53ec5389c50df14f7682a1151aa4edb843f
VihanR <163058165+saquer916@users.noreply.github.com>
2025-12-27 02:42:46 -0500

    added eventlet for more secure deployment

--- main.py
+++ main.py
@@ -114,4 +114,5 @@ def disconnect():
     print(f"{name} has left room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000, server_options={"async_mode": "eventlet"})
+
