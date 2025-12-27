0e241583de37b97a1ee3deb597933f8d6c82db59
VihanR <163058165+saquer916@users.noreply.github.com>
2025-12-27 02:49:43 -0500

    add port binding

--- main.py
+++ main.py
@@ -114,5 +114,8 @@ def disconnect():
     print(f"{name} has left room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000, server_options={"async_mode": "eventlet"})
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    socketio.run(app, debug=True, host="0.0.0.0", port=port, server_options={"async_mode": "eventlet"})
+
 
