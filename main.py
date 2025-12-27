6cf906a9c6efdd1de4c024a1c887943998aa4264
vihan_0v4rt5t <vihanremotelearning@gmail.com>
2025-12-27 01:33:12 -0500

    added code to diminish hard-coded PORT

--- main.py
+++ main.py
@@ -114,4 +114,6 @@ def disconnect():
     print(f"{name} has left room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
